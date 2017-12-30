f963f304d6bd9985146ae72dd0917ce09ee18fd0
Mike Dvorscak <mike.dvorscak@gmail.com>
2017-12-30 13:28:18 -0600

    Remove materialize

--- src/App.vue
+++ src/App.vue
@@ -23,4 +23,24 @@ export default {
   margin: 0 auto;
   width: 75vw;
 }
+html {
+    font-family: "Roboto", "Droid Sans", "Open Sans", sans-serif;
+    font-weight: normal;
+    color: rgba(0,0,0,0.87);
+}
+body{
+  margin: 0;
+}
+a{
+  text-decoration: none;
+  -webkit-tap-highlight-color: transparent;
+  background-color: transparent;
+}
+img {
+  border: 0;
+}
+*, *:before, *:after{
+    -webkit-box-sizing: inherit;
+    box-sizing: inherit;
+}
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -20,8 +20,28 @@ export default {
 };
 </script>
 <style>
+@media only screen and (min-width: 601px) {
+  .navbar-fixed {
+    height: 64px;
+    line-height: 64px;
+  }
+}
+.navbar-fixed {
+  position: relative;
+  height: 56px;
+  z-index: 997;
+}
+.navbar-fixed nav {
+    position: fixed;
+}
 nav{
+  color: #FFF;
+  width: 100%;
   background-color: #27ae60;
+  height: 56px;
+  line-height: 56px;
+  box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 1px 5px 0 rgba(0,0,0,0.12), 0 3px 1px -2px rgba(0,0,0,0.2);
+  display: block;
 }
 .nav-wrapper{
   margin: 0 auto;
@@ -32,6 +52,28 @@ nav{
   display: inline-flex;
   height: 100%;
   flex-direction: row;
+  padding-left: 0;
+  list-style-type: none;
+  margin: 0;
+}
+.nav-wrapper ul li{
+  list-style-type: none;
+  -webkit-transition: background-color .3s;
+  transition: background-color .3s;
+  float: left;
+  padding: 0;
+}
+nav ul a {
+    -webkit-transition: background-color .3s;
+    transition: background-color .3s;
+    font-size: 1rem;
+    color: #fff;
+    display: block;
+    padding: 0 15px;
+    cursor: pointer;
+}
+nav ul a:hover {
+    background-color: rgba(0,0,0,0.1);
 }
 .nav-wrapper ul a{
   font-size: 3vw;
