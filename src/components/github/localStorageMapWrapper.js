6aaa698ea30de89cfb5cb3dc7c159be320cbc0aa
Mike Dvorscak <mike.dvorscak@gmail.com>
2018-01-18 23:06:27 -0600

    Using real map wrapper now
    TODO: extract mem map facade to own package

--- src/components/github/localStorageMapWrapper.js
+++ src/components/github/localStorageMapWrapper.js
@@ -1,31 +1,21 @@
-// TODO: Extract to own package
-import isPromise from 'p-is-promise';
+import lsMapWrap from 'ls-map-wrap';
+import cloak from 'cloakjs';
 
-const ls = window.localStorage;
+/**
+ * mem will always set with val.data and val.maxAge
+ */
+cloak(lsMapWrap, 'set').cloakWith((key, val, orginalFn) => {
+  orginalFn(`${key}-data`, val.data);
+  orginalFn(`${key}-maxAge`, val.maxAge);
+});
 
+cloak(lsMapWrap, 'get').cloakWith((key, orginalFn) => {
+  const recontructedObj = {};
+  recontructedObj.data = orginalFn(`${key}-data`);
+  recontructedObj.maxAge = orginalFn(`${key}-maxAge`);
+  return recontructedObj;
+});
 
-export default {
-  has(key) {
-    return ls.getItem(key) !== null;
-  },
-  get(key) {
-    return JSON.parse(ls.getItem(key));
-  },
-  set(key, value) {
-    if (isPromise(value.data)) {
-      value.data.then((innerData) => {
-        const valueToSave = value;
-        valueToSave.data = innerData;
-        ls.setItem(key, JSON.stringify(valueToSave));
-      });
-    } else {
-      ls.setItem(key, JSON.stringify(value));
-    }
-  },
-  delete(key) {
-    return ls.removeItem(key);
-  },
-  clear() {
-    return ls.clear();
-  }
-};
+cloak(lsMapWrap, 'has').cloakWith((key, orginalFn) => orginalFn(`${key}-data`) && orginalFn(`${key}-maxAge`));
+
+export default lsMapWrap;
