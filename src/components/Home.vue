f76231da446c9b7434a21529047d3992fed7b991
Mike Dvorscak <mike.dvorscak@gmail.com>
2018-01-28 17:42:30 -0600

    Switch to stylus

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,37 +10,32 @@
 </template>
 
 <script type="text/javascript">
-
-export default {
-};
+export default {};
 </script>
-<style>
-.home img{
-  max-width: 100%;
-  height: auto;
-  width: auto;
-  display: block;
-  margin: 0 auto;
-}
-.image-container{
-  position: relative; 
-  width: 100%;
-  display: block;
-  margin: 0 auto;
-}
-.home h3{
-  position: absolute; 
-  bottom: 30%; 
-  left: 0; 
-  width: 100%; 
-  text-align: center;
-  background: white;
-  padding: 11px;
-  font-size: 3vw;
-}
-@media (min-width: 1400px) { 
-  .home h3{
-    font-size: 42px; /* 1400 * 3vw (1400 * .03) */
-  }
-}
+<style lang="stylus">
+.home 
+  img
+    max-width 100%
+    height auto
+    width auto
+    display block
+    margin 0 auto
+  &.image-container
+    position relative
+    width 100%
+    display block
+    margin 0 auto
+  h3
+    position absolute
+    bottom 30%
+    left 0
+    width 100%
+    text-align center
+    background white
+    padding 11px
+    font-size 3vw
+
+@media (min-width: 1400px)
+  .home h3
+    font-size 42px /* 1400 * 3vw (1400 * .03) */
 </style>
