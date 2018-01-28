d92357a9536d3eb2327bb9506823fe0c8318c14f
Mike Dvorscak <mike.dvorscak@gmail.com>
2018-01-28 17:40:16 -0600

    Convert to stylus

--- src/App.vue
+++ src/App.vue
@@ -18,29 +18,28 @@ export default {
 };
 </script>
 
-<style>
-#content {
-  margin: 0 auto;
-  width: 75vw;
-}
-html {
-    font-family: "Roboto", "Droid Sans", "Open Sans", sans-serif;
-    font-weight: normal;
-    color: rgba(0,0,0,0.87);
-}
-body{
-  margin: 0;
-}
-a{
-  text-decoration: none;
-  -webkit-tap-highlight-color: transparent;
-  background-color: transparent;
-}
-img {
-  border: 0;
-}
-*, *:before, *:after{
-    -webkit-box-sizing: inherit;
-    box-sizing: inherit;
-}
+<style lang="stylus">
+#content
+  margin 0 auto
+  width 75vw
+
+html
+  font-family 'Roboto', 'Droid Sans', 'Open Sans', sans-serif
+  font-weight normal
+  color rgba(0, 0, 0, 0.87)
+
+body
+  margin 0
+
+a
+  text-decoration none
+  -webkit-tap-highlight-color transparent
+  background-color transparent
+
+img
+  border 0
+
+*, *:before, *:after
+  -webkit-box-sizing inherit
+  box-sizing inherit
 </style>
