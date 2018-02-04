bbe14450a9152353180642a914b9c0824e592ad6
Mike Dvorscak <mike.dvorscak@gmail.com>
2018-02-04 16:42:36 -0600

    Refactor common code pug parts to mixins

--- src/components/github/GitHubRepo.vue
+++ src/components/github/GitHubRepo.vue
@@ -1,25 +1,29 @@
 <template lang="pug">
-  a.repo-item(:href="repo.html_url")
-    div.repo-content
-      div.repo-title
-        span.text-holder-wrapper(v-text="repo.name")
-      div.repo-homepage(v-if="repo.homepage") Project site: 
-        a(:href="repo.homepage" v-text="repo.homepage")
-      div.repo-desc
-        span.text-holder-wrapper(v-text="repo.description")
-      div.repo-info
-        div.repo-language
-          span.text-holder-wrapper(v-text="repo.language")
-        div.repo-stats
-          svg.octicon.octicon-eye(aria-hidden="true" class="" height="16" version="1.1" viewBox="0 0 16 16" width="16")
-            path(fill-rule="evenodd" d="M8.06 2C3 2 0 8 0 8s3 6 8.06 6C13 14 16 8 16 8s-3-6-7.94-6zM8 12c-2.2 0-4-1.78-4-4 0-2.2 1.8-4 4-4 2.22 0 4 1.8 4 4 0 2.22-1.78 4-4 4zm2-4c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z")
-          p(v-text="repo.watchers_count")
-          svg.octicon.octicon-star(aria-hidden="true" class="" height="16" version="1.1" viewBox="0 0 14 16" width="14")
-            path(fill-rule="evenodd" d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74z")
-          p(v-text="repo.stargazers_count")
-          svg.octicon.octicon-repo-forked(aria-hidden="true" class="" height="16" version="1.1" viewBox="0 0 10 16" width="10")
-            path(fill-rule="evenodd" d="M8 1a1.993 1.993 0 0 0-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 0 0 2 1a1.993 1.993 0 0 0-1 3.72V6.5l3 3v1.78A1.993 1.993 0 0 0 5 15a1.993 1.993 0 0 0 1-3.72V9.5l3-3V4.72A1.993 1.993 0 0 0 8 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z")
-          p(v-text="repo.forks")
+mixin text-block(text)
+  div(class!= attributes.class)
+    span.text-holder-wrapper(v-text!= text)
+mixin stat(text, d)
+  svg.octicon(aria-hidden="true")
+    path(fill-rule="evenodd" d!= d)
+  p(v-text!= text)
+a.repo-item(:href="repo.html_url")
+  div.repo-content
+    +text-block("repo.name")(class="repo-title")
+    div.repo-homepage(v-if="repo.homepage") Project site: 
+      a(:href="repo.homepage" v-text="repo.homepage")
+    +text-block("repo.description")(class="repo-desc")
+    div.repo-info
+      +text-block("repo.language")(class="repo-language")
+      div.repo-stats
+        +stat("repo.watchers_count", `M8.06 2C3 2 0 8 0 8s3 6 8.06 6C13 14 16 8 16 8s-3-6-7.94-6zM8 12c-2.2 
+                              0-4-1.78-4-4 0-2.2 1.8-4 4-4 2.22 0 4 1.8 4 4 0 2.22-1.78 4-4 4zm2-4c0 1.11-.89 
+                              2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z`)
+        +stat("repo.stargazers_count", "M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74z")
+        +stat("repo.forks", `M8 1a1.993 1.993 0 0 0-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 0 0 2 1a1.993 1.993 
+                              0 0 0-1 3.72V6.5l3 3v1.78A1.993 1.993 0 0 0 5 15a1.993 1.993 0 0 0 1-3.72V9.5l3-3V4.72A1.993 
+                              1.993 0 0 0 8 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 
+                              1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 
+                              1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z`)
 </template>
 
 <script type="text/javascript">
@@ -72,4 +76,8 @@ repo-item-gray = #5c6877
   display inline-flex
   p
     margin 0 15px
+
+.octicon
+  width 16px
+  height 16px
 </style>
